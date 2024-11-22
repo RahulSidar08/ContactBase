23c903823b4fdbb3bb172d62b9d91e1f3f7424d1
Rahul Sidar <150533760+RahulSidar08@users.noreply.github.com>
2024-11-22 12:23:27 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,11 +4,7 @@ const {getContact,getContactwithId, createContact,updateContact,deleteContact} =
 const validateToken = require("../middleware/validateTokenHandler");
 
 
-// router.get("/", getContact)
-// router.get("/:id",getContactwithId);
-// router.post("/",createContact);
-// router.put("/:id",updateContact);
-// router.delete("/:id",deleteContact);
+
 
 router.use(validateToken)
 router.get("/", getContact).post("/",createContact);
