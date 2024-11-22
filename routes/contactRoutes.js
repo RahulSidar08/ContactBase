4afabbc0289ba5ad81850e1cd5c8c14c2a46caf4
Rahul Sidar <150533760+RahulSidar08@users.noreply.github.com>
2024-11-22 12:23:39 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const {getContact,getContactwithId, createContact,updateContact,deleteContact} = require("../controllers/contactControllers");
 const validateToken = require("../middleware/validateTokenHandler");
 
-
-
-
 router.use(validateToken)
 router.get("/", getContact).post("/",createContact);
 router.get("/:id",getContactwithId).put("/:id",updateContact).delete("/:id",deleteContact);
